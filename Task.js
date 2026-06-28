db2d92bf2826a7dabb1fdb540f577db1197f5854
Shalu yadav <saluyadav102@gmail.com>
2026-06-28 21:25:23 +0530

    Add files via upload

---
+++ Task.js
@@ -0,0 +1,738 @@
+// // //TASK//
+// // // 10 qus logical operator
+// // //type conversion and  type casting 2 qus
+// // //5 qus  for loops 5 while loop
+// // //how to create infinite
+
+// // //pre & post increament 
+// // // type of console
+
+
+// // //Logical operator
+
+// // console.log(10 && 20);//20
+// // console.log(0||100);//100
+// // console.log(!"Hello");//false
+// // console.log(5 > 3 && 10 < 5);//false
+// // console.log(null || undefined || "JS");
+// // console.log(100||561||788);
+
+
+// // let a = "";
+// // let b = "MERN";
+// // console.log(a && b);
+// // console.log(a||b);//MERN
+
+
+// // let x = 0;
+// // let y = 5;
+// // console.log(x || y && 10);//10
+
+// // console.log(Boolean("0") && Boolean(0));//false
+
+
+// // console.log(1 && 2 && 3 && 0 && 5);//0
+
+// // let a=10>5
+// // let b=5<15
+// // console.log(a && b);//true
+
+
+// // console.log(!null);//true
+
+
+// // let user=null;
+// // console.log(user||"guest");//guest
+
+// // console.log([] && {} && "JS");//JS
+// // console.log(0 || false || "" || "MERN");//MERN
+// // console.log(10 && 20 || 30 && 40);
+// // console.log(![] && !{} || !!"Hello");
+
+// // let a = null;
+// // let b = "JavaScript";
+// // console.log(a && b || !a);
+
+// // let b=null;
+// // console.log(!b);
+
+
+// // console.log(10&&100);
+
+// // let x = 0;
+// // let y = 5; 
+
+// // console.log(x || y && 10);
+
+
+// //Type conversion
+
+
+// // console.log("5"+569);//5569
+
+// // console.log("56"-6);//50
+
+// // TYPE casting
+
+// // let num=56;
+// // let str=String(num)
+// // console.log(str);
+
+
+// // let n="56"
+// // let num=Number(n)
+// // console.log(num);
+
+
+// // console.log(Boolean(0));
+
+// //FOR LOOP
+// // for (let i = 1; i <=100; i+=2) {//ODD
+// //     console.log(i);
+// // }
+
+// // for (let i=1;i<10;i+=2){
+// //     console.log(i);
+// // }
+
+// //Task required
+
+// // for(let i=97; i<=102; i++){
+// //     console.log(String.fromCharCode(i));
+// // }
+
+
+// // for(let i=65;i<=90;i++){
+// //     console.log(String.fromCharCode(i));
+// // }
+
+// //WHILE LOOP
+
+// // let i=1;
+// // while(i<=5){
+// //     console.log(i);
+// //     i++
+// // }
+
+// //infinte loop
+
+
+// // let i=1
+// // while(i<=5){
+// //     console.log(i);
+// // }
+
+
+
+
+// // Q1
+// // Using a for loop, print numbers from 1 to 10.
+
+// // for (let i = 1; i <=10; i++) {
+// //     console.log(i);
+// // }
+
+// // Q2
+
+// // Using a while loop, print numbers from 10 to 1.
+// //  let i=10;
+// //  while(i>=1){
+// //     console.log(i);
+// //     i--
+// //  }
+// // Q3
+
+// // Using a for loop, print all even numbers from 2 to 20.
+
+// // for (let i = 2; i <= 20; i+=2) {
+// //     console.log(i);
+    
+// // }
+
+// // Q4
+
+// // Using a while loop, print all odd numbers from 1 to 15.
+
+
+// // let i=1
+// // while (i<=15) {
+// //     console.log(i);
+// //     i+=2
+// // }
+
+// // Q5
+
+// // Using a for loop, calculate and print the sum of numbers from 1 to 10.
+// // let sum=0
+// // for (let i = 1; i <=10; i++) {
+// //     sum+=i
+// // }
+// // console.log(sum);
+// // Expected Output:
+
+// // 55
+// // Q6
+
+// // Using a while loop, calculate and print the sum of even numbers from 2 to 10.
+
+
+// // let i=2
+// // let sum=0
+// // while(i<=10){
+// //     sum+=i
+// //     i+=2
+// // }
+// // console.log(sum);
+
+// // Expected Output:
+
+// // 30
+// // Q7
+
+// // Using a for loop, print:
+
+// // for (let i = 65; i <=69; i++) {
+// //     console.log(String.fromCharCode(i));
+    
+// // }
+
+// // A
+// // B
+// // C
+// // D
+// // E
+// // Q8
+
+// // Using a while loop, print:
+// // let i=5;
+// // while(i<=25){
+// //     console.log(i);
+// //     i+=5
+// // }
+// // 5
+// // 10
+// // 15
+// // 20
+// // 25
+// // Q9 (Interview Level)
+
+// // Using a for loop, print the multiplication table of 5.
+// // for (let i = 1; i <=10; i++) {
+// //     let res=5*i
+// //     console.log(`5 x ${i}= ${res}`);
+// // }
+// // Output:
+
+// // 5 x 1 = 5
+// // 5 x 2 = 10
+// // ...
+// // 5 x 10 = 50
+// // Q10 (Interview Level)
+
+// // Using a while loop, count how many numbers are there from 1 to 100 and print the count.
+// // let count=0
+// // let i=1
+// // while(i<=100){
+// //     count++
+// //     i++
+// // }
+// // console.log(count);
+
+
+// //pre increment 
+
+// // let i=10
+// // console.log(++i);
+// // console.log(i);
+// // //post increment
+// // console.log(i++);
+// // console.log(i);
+
+
+// // //pre decrement
+// // let i=2
+// // console.log(--1);
+// // console.log(i);
+// // //post drecremernt
+
+// // let i=20;
+// // console.log(i--);
+// // console.log(i);
+
+
+// //Types of console
+
+// // 1.console.log("General message");
+// // 2.console.log("Good morning")
+// // console.log("Hello ");
+
+// // let user={
+// //     name:"Shalu",
+// //     course:"B.Tech",
+// //     fee:60000
+// // }
+// // console.log(user);
+
+// // 2.console.error("error message");
+// let a=0
+// let b=8
+// let c=b/a
+
+//  console.error("error divide by zero")
+
+
+
+// // 3.console.warn("warning message");
+
+// // console.warn("low battery")
+
+// // 4.console.info("information message");
+
+// // console.info("userr log in");
+
+// // 5.console.table(" table format of array object method");
+
+
+// // let user={
+// //     name:"Shalu",
+// //     course:"B.Tech",
+// //     fee:60000
+// // }
+// // console.table(user)
+
+// // 6.console.clear("clear message");
+
+
+
+// // let user={
+// //     name:"Shalu",
+// //     course:"B.Tech",
+// //     fee:60000
+// // }
+// // console.clear(user)
+
+
+
+
+
+
+// // const smallest=(a,b)=>{
+// //     return a<b ? "smaller a" : "smaller b"
+// // }
+// // console.log(smallest(4,6));
+
+
+
+
+// // //Create an arrow function that takes a number and returns its square.
+
+// const square=(n)=>{
+//     return n*n
+// }
+// console.log(square(5));
+
+// // Create an arrow function that takes two numbers and returns the larger number.
+
+// const greatrer=(a,b)=>{
+//     return a>b ? "greater a" : "greater b"
+// }
+// console.log(greatrer(4,6));
+
+// // Create an arrow function that checks whether a number is even or odd.
+// const even=(num)=>{
+//     return num%2==0 ? "even": "odd"
+// }
+// console.log(even(5));
+
+
+// // Create an arrow function that takes a string and returns its length.
+// const StrLen=(str)=>{
+//     return str.length
+// }
+// console.log(StrLen("Shalu"));
+
+// // Create an arrow function that takes a student's marks and returns "Pass" if marks are 40 or above, otherwise "Fail".
+//  const Student=(marks)=>{
+//     return marks>=40 ? "Pass":"Fail"
+//  }
+// console.log(Student(56));
+
+// // Create an arrow function that takes three numbers and returns the smallest number.
+// const smallest=(a,b,c)=>{
+//     return a<b && a<c ? "smallest a": "smallest b"
+// }
+// console.log(smallest(4,2,7));
+
+// // Create an arrow function that calculates the area of a rectangle using length and width.
+
+// const areaOfRec=(l,w)=>{
+//     return l*w
+// }
+// console.log(areaOfRec(4,5));
+// // Create an arrow function that takes a string and returns it in uppercase.
+// const string=(str)=>{
+//     return str.toUpperCase()
+// }
+// console.log(string("shalu"));
+// // Create an arrow function that takes an array of numbers and returns the sum of all elements.
+// const ars=(arr)=>{
+//     let sum=0
+//     for (let i = 0; i < arr.length; i++) {
+//         sum+=arr[i]
+//     }
+//         return sum
+
+// }
+// let n=[1,2,3,4,5,6,7,8,9]
+// console.log(ars(n));
+
+
+// // Create an arrow function that takes a number and returns its factorial.
+
+// // Create an arrow function that counts the number of vowels in a string.
+
+// // Create an arrow function that reverses a given string.
+
+// // Create an arrow function that checks whether a given string is a palindrome.
+
+// // Create an arrow function that takes an array and returns the largest element.
+
+// // Create an arrow function that converts temperature from Celsius to Fahrenheit.
+// const CelToFr=(celcius)=>{
+//     return celcius*1.8 +32
+// }
+// console.log(CelToFr(33));
+
+// // Create an arrow function that takes an array of numbers and returns only the even numbers.
+
+// const evenNum=(arr)=>{
+//     let even=[]
+//     for (let i = 0; i < arr.length; i++) {
+//         if(arr[i]%2===0){
+//             even.push(arr[i])
+//         }
+//     }
+//     return even;
+// }
+// let num=[1,2,3,4,5,6,7,8,9]
+// console.log(evenNum(num));
+
+// // Create an arrow function that takes a name and returns:
+// "Hello, <name>! Welcome to CodingClave."
+// const LLP=(name)=>{
+//     return "Hello"+" "+name+" "+"!welcome to CodingClave"
+// }
+// console.log(LLP("Shalu"));
+
+// // Create an arrow function that calculates the simple interest using Principal, Rate, and Time.
+// const SI=(p,r,t)=>{
+//     return (p*r*t)/100
+// }
+// console.log(SI(5,6,8));
+// // Create an arrow function that checks whether a number is prime or not.
+
+// const isPrime=(num)=>{
+//     if (num <=1){
+//         return "Not Prime"
+//     }
+//     for(let i=2;i<num;i++){
+//         if(num % i===0){
+//             return "Not prime"
+//         }
+//     }
+//     return "prime No"
+//     }
+// console.log(isPrime(4));
+// console.log(isPrime(2));
+
+// // Create an arrow function that takes an array of marks and returns the average marks.//
+
+//  let $num=46
+//  console.log($num);
+
+//  let num=565
+//  console.log(typeof(num));
+
+//  let i=1
+
+//  console.log(i++);
+//   console.log(++i);
+
+//   user={
+//     name:"Shalu",
+//     course:"B.Tech"
+//   }
+//   console.log(user);
+//   console.log(typeof(user));
+
+// let x=10
+// x+=5
+// console.log(x);
+
+// let a=5
+// let b=6
+// console.log(a!==b)
+
+// // let age=144
+// // let canVote= (age>18)?"can vote": "cannot vote"
+// // console.log(canVote);
+
+// let num=15
+// if(num%2==0){
+//     console.log("even");
+// }else{
+//     console.log("odd");
+// }
+
+
+// let marks=85
+// if (marks>=33) {
+//     console.log("Pass");
+// } else {
+//     console.log("Fail");
+// }
+
+// if ((Username = "admin")&& (Password = "1234")) {
+//     console.log("Login Successful");
+// } else {
+//     console.log("invalid credrential");
+// }
+
+// let day=2
+// switch(day){
+//     case 1:
+//         console.log("Sunday");
+//         break
+//     case 2:
+//         console.log("monday");
+//         break
+//     default:
+//         console.log("invalid");
+
+
+// }
+
+// let marks=95
+// switch(true){
+//     case marks>90 && marks<100:
+//         console.log("Excellent");
+//         break
+//     case marks>80 && marks<90:
+//         console.log("Very Good");
+//         break
+//     case marks>70 && marks<80:
+//         console.log(" Good");
+//         break
+//     case marks>60 && marks<70:
+//         console.log("Pass");
+//         break
+//     default:
+//         console.log("Fail");
+// }
+    
+
+// let marks = 8;
+
+// switch (true) {
+//     case marks >= 90:
+//         console.log("Excellent");
+//         break;
+//     case marks >= 75:
+//         console.log("Very Good");
+//         break;
+//     case marks >= 60:
+//         console.log("Good");
+//         break;
+//     case marks >= 40:
+//         console.log("Pass");
+//         break;
+//     default:
+//         console.log("Fail");
+// }
+
+
+// let age=156
+// switch(true){
+//     case age>=0 && age<=12:
+//         console.log("Child");
+//         break
+//     case age>13 && age<19:
+//         console.log("Teenager");
+//         break
+//     case age>20 && age<59:
+//         console.log("Adult");
+//         break
+//     default:
+//         console.log("Senior citizen");
+// }
+
+// setTimeout(() =>{
+//     console.log("welcome");
+// },3000)
+// console.log(3);
+
+//
+
+//Interview level qus
+
+// 1
+
+
+// const sumOdd=(n)=>{
+//     let sum=0
+//     for (let i = 1; i < n; i++) {
+//         if (i%2!==0){
+//             sum+=i
+//         }
+//     }
+//     return sum
+// }
+// console.log(sumOdd(9));
+
+//2
+
+
+
+
+// const countPositive=(num)=>{
+//     for (let i = 0; i < num.length; i++) {
+//         if (num<0){
+//             count++
+//         }     
+//     }
+//     return count
+// }
+// console.log(countPositive[-2,5,0,8,-1,10]);
+
+
+
+// for(let i=1;i<=5;i++){
+//     let star=""
+//     for (let j = 1; j < i; j++) {
+//         star+="*"
+//     }
+//     console.log(star);
+// }
+
+
+// let str="javaScript"
+// console.log(str.slice(0,5));
+
+// let name="Ram Shyam karan"
+// // let arr=name.split("-")
+// // console.log(arr);
+// console.log(name.split("-"))
+
+//escapecharacterr
+
+// console.log("Hello\nWorld");
+// console.log("Hello\tWorld");
+
+// let str="ShaluYadav"
+// console.log(str.indexOf("a"));
+
+// const add=function(a,b){
+//     return a+b
+// }
+// console.log(add(6,5));
+
+//fun declaration
+// function greet(){
+//     console.log("hello");
+// }
+// greet()
+
+// function greet(name){
+//     return "Hlo "+name
+// }
+// console.log(greet("Shalu"));
+
+// function sayhi(){
+//     console.log("Hii");
+// }
+// sayhi()
+
+
+// const msg=(name)=>{
+//     return "Hello"+ " "+name
+// }
+// // console.log(msg("Shalu"));
+// //1
+// const greater=(a,b)=>{
+//     return a>b ? a:b
+// }
+// console.log(greater(10,25));
+
+// //2
+
+// const getLength=(str)=>{
+//     return str.length
+// }
+// console.log(getLength("JavaScript"));
+
+// //3
+
+// const evenNumbers=(arr)=>{
+//     let even=[]
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i]%2===0) {
+//             even.push(arr[i])
+//         }
+//     }
+//     return even
+// }
+// console.log(evenNumbers([2,5,8,11,14]));
+
+// //4
+
+// const isPrime=(num)=>{
+//      if (num<=1) {
+//             return "Not Prime"
+//         }
+//     for (let i = 2; i <num; i++) {
+//         if(num%i===0){
+//             return "Not prime"
+//         }
+//     }
+//     return "prime"
+// }
+// console.log(isPrime(17));
+// console.log(isPrime(18));
+
+// //5
+
+// const largest=(arr)=>{
+//     let largest=arr[0]
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i]>largest){
+//             largest=arr[i]
+//         }
+//     }
+//     return largest
+// }
+// let n=[10,45,8,99,21]
+// console.log(largest(n));
+// //6
+// const reverse=(str)=>{
+//     let rev=""
+//     for (let ch of str) {
+//         rev=ch+rev
+//     }
+//     return rev
+// }
+// console.log(reverse("hello"));
+
+//7
+
+// const palindrome=(str)=>{
+//     let rev=""
+//     for(let ch of str){
+//         rev=ch+rev
+//     }
+//     if (rev===str) {
+//         return "Palindrome"
+//     } else {
+//         return "not Palindrome"
+//     }
+// }
+// console.log(palindrome("madam"));
